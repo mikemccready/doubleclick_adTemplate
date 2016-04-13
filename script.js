744bfe4a475bfef69fd2c8cc90c80ccb8cea6175
Mike McCready <michaeljmccready@gmail.com>
2016-04-13 10:44:24 -0700

    initial banner ad template

---
+++ script.js
@@ -0,0 +1,32 @@
+// Start Enable initialization ///////////////////////////////////////////////////////////
+if (!Enabler.isInitialized()) {
+	Enabler.addEventListener(studio.events.StudioEvent.INIT, enablerInitialized);
+} else {
+   enablerInitialized();
+}
+function enablerInitialized() {
+	// Enabler initialized. In App ads are rendered offscreen so animation should wait for
+	// the visible event. These are simulated with delays in the local environment.
+	if (!Enabler.isVisible()) {
+		Enabler.addEventListener(studio.events.StudioEvent.VISIBLE, adVisible);
+	} else {
+		adVisible();
+	}
+}// End Enabler Initialization ///////////////////////////////////////////////////////////
+
+
+// Start Ad
+function adVisible() {
+  console.log('here');
+}
+// End Ad Init
+
+// Enabler Exits ////////////////////////////////////////////////////////////////////////
+Enabler.exit("Click on BG");
+// Exits; overriding exit.
+// Enabler.exitOverride("Click on BG", "http://www.google.com");
+// Counters.
+// Enabler.counter("Clicked on tab 1");
+// Timers
+// Enabler.startTimer("Spinning car");
+// Enabler.stopTimer("Spinning car");
